e1142700bd8c7aa0679f51e5984f9613988ef602
Dennis <deweis@gmx.ch>
2019-01-04 13:13:31 +0100

    Add option to show a second plant image

---
+++ script.js
@@ -0,0 +1,10 @@
+/* Show next picture of the plant */
+document.getElementById('violaTricolor').addEventListener('click', function() {
+  this.children[0].src = 'img/Viola_tricolor_styvmorsviol.jpg';
+});
+
+document
+  .getElementById('corylusAvellana')
+  .addEventListener('click', function() {
+    this.children[0].src = 'img/Corylus_avellana0.jpg';
+  });
